0e331ed0154ed0fb9b4acfb7de6ab97c7c6271f1
Harry Mumford-Turner <harrymt@me.com>
2017-02-13 14:34:45 +0000

    Split Scss files into proper directories

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
 
     scsslint: {
       all: [
-        'dev/scss/**/*.scss',
+        'dev/scss/_partials/*.scss',
         'dev/scss/*.scss'
       ]
     },
