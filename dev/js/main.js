c442e92588951d74ea3afcbe7d3a19d17c57fe0b
Harry Mumford-Turner <harrymt@me.com>
2017-03-16 17:02:17 +0000

    Separated all JS
    Removed unused util class

--- dev/js/main.js
+++ dev/js/main.js
@@ -5,73 +5,13 @@
 (function() {
   "use strict";
 
-  // var util = require('util.js'); // TODO change front end js things to not be combined?
-  // TODO look into other front end projects to see how they did it
+  $(document).ready(function() {
+    handleHits();
+  });
 
-  // Animation variables
-  var WIDTH = 200;
-  var HEIGHT = 200;
-  var buildingsY = 0;
-  var rain_size = 20;
-  var blueSquares = [];
-  var droplets = (WIDTH / (rain_size + 5));
-
-  function draw(canvas) {
-      canvas.width = WIDTH; canvas.height = HEIGHT;
-      var context = canvas.getContext("2d");
-      // Setup shadow
-      context.shadowColor = "rgba(0, 0, 0, 0.3)";
-      context.shadowBlur = 20;
-      context.shadowOffsetX = 10;
-      context.shadowOffsetY = 10;
-
-      for(var i = 0; i < droplets; i++) {
-        blueSquares.push(utils.getRandomNum(0, 100));
-      }
-
-      return context;
-  }
-
-  function moveCubes(context) {
-    buildingsY++; if(buildingsY > (WIDTH * 2)) { buildingsY = -WIDTH; }
-    for(var i = 0; i < droplets; i++) {
-      blueSquares[i]++;
-      if(blueSquares[i] > HEIGHT) { blueSquares[i] = 0; }
-    }
-  }
-
-  function update(context) {
-      context.clearRect(0, 0, WIDTH, HEIGHT);
-
-      moveCubes(context);
-
-      // Draw buildings
-      context.fillStyle = "#ddd";
-      var big_size = 40;
-      var small_size = 20;
-      context.fillRect(buildingsY + 90, HEIGHT - small_size, 20, small_size);
-      context.fillRect(buildingsY + 50, HEIGHT - small_size, 20, small_size);
-      context.fillRect(buildingsY, HEIGHT - big_size, 20, big_size);
-      context.fillRect(buildingsY - 40, HEIGHT - big_size, 20, big_size);
-      context.fillRect(buildingsY - 80, HEIGHT - small_size, 20, small_size);
-      context.fillRect(buildingsY - 150, HEIGHT - big_size, 20, big_size);
-      context.fillRect(buildingsY - 200, HEIGHT - small_size, 20, small_size);
-
-      // Draw rain
-      context.fillStyle = "rgba(0, 0, 50, 0.5)";
-      var x = 0;
-      for(var y = 0; y < droplets; y++) {
-        context.beginPath();
-        context.fillRect(x, y + blueSquares[y], rain_size, rain_size);
-        x += rain_size + 10;
-      }
-  }
-
-
-
-
-
-
+  /**
+   * Make a hit and load the current hits.
+   */
   function handleHits() {
 
     /**
@@ -86,51 +26,9 @@
      */
     $.post("/hit", function( data ) {
       if(!data) {
-        console.log("Error unable to count number of hits.")
+        console.log("Error unable to count number of hits.");
       }
     });
   }
-
-  if (typeof window != "undefined") {
-    window.onload = function() {
-      handleHits();
-
-      var canvas = document.getElementById("png-example");
-      if(doesElementExist(canvas)) {
-        var context = draw(canvas);
-        setInterval(function() { update(context); }, 20);
-      }
-    }
-  }
-
-  /**
-   * Generate a random number between the min and the max.
-   */
-  function getRandomNum(min, max) {
-    return Math.round(Math.random() * (max - min + 1)) + min;
-  }
-
-  /**
-   * Checks if the given element object exists.
-   */
-  function doesElementExist(el) {
-    return (typeof(el) != 'undefined' && el != null);
-  }
-
-
-  // /**
-  //  * Generate a random number between the min and the max.
-  //  */
-  // exports.getRandomNum = function(min, max) {
-  //   return Math.round(Math.random() * (max - min + 1)) + min;
-  // };
-
-  // /**
-  //  * Checks if the given element object exists.
-  //  */
-  // exports.doesElementExist = function(el) {
-  //   return (typeof(el) != 'undefined' && el != null);
-  // };
-
 
 })();
--- dev/js/svg-animation.js
+++ dev/js/svg-animation.js
@@ -1,11 +1,9 @@
-"use strict";
 
-// TODO NOT IMPLEMENTED require('util.js');
-
-
-// TODO NOT YET IMPLEMENTED
-// SVG animation module
+//
+// SVG animation
+//
 (function() {
+    "use strict";
 
     // Animation variables
     var WIDTH = 200;
@@ -15,30 +13,39 @@
     var blueSquares = [];
     var droplets = (WIDTH / (rain_size + 5));
 
+    /**
+     * Draw the animation.
+     */
     function draw(canvas) {
-        canvas.width = WIDTH; canvas.height = HEIGHT;
-        var context = canvas.getContext("2d");
-        // Setup shadow
-        context.shadowColor = "rgba(0, 0, 0, 0.3)";
-        context.shadowBlur = 20;
-        context.shadowOffsetX = 10;
-        context.shadowOffsetY = 10;
+          canvas.width = WIDTH; canvas.height = HEIGHT;
+          var context = canvas.getContext("2d");
+          // Setup shadow
+          context.shadowColor = "rgba(0, 0, 0, 0.3)";
+          context.shadowBlur = 20;
+          context.shadowOffsetX = 10;
+          context.shadowOffsetY = 10;
 
-        for(var i = 0; i < droplets; i++) {
-          blueSquares.push(getRandomNum(0, 100));
-        }
+          for(var i = 0; i < droplets; i++) {
+            blueSquares.push(utils.getRandomNum(0, 100));
+          }
 
-        return context;
+          return context;
     }
 
+    /**
+     * Move the cubes array.
+     */
     function moveCubes(context) {
-      buildingsY++; if(buildingsY > (WIDTH * 2)) { buildingsY = -WIDTH; }
-      for(var i = 0; i < droplets; i++) {
-        blueSquares[i]++;
-        if(blueSquares[i] > HEIGHT) { blueSquares[i] = 0; }
-      }
+        buildingsY++; if(buildingsY > (WIDTH * 2)) { buildingsY = -WIDTH; }
+        for(var i = 0; i < droplets; i++) {
+            blueSquares[i]++;
+            if(blueSquares[i] > HEIGHT) { blueSquares[i] = 0; }
+        }
     }
 
+    /**
+     * Animate the scene.
+     */
     function update(context) {
         context.clearRect(0, 0, WIDTH, HEIGHT);
 
@@ -60,9 +67,42 @@
         context.fillStyle = "rgba(0, 0, 50, 0.5)";
         var x = 0;
         for(var y = 0; y < droplets; y++) {
-          context.beginPath();
-          context.fillRect(x, y + blueSquares[y], rain_size, rain_size);
-          x += rain_size + 10;
+            context.beginPath();
+            context.fillRect(x, y + blueSquares[y], rain_size, rain_size);
+            x += rain_size + 10;
         }
     }
-});
+
+
+    /**
+     * Start animation if the animation exists on the page.
+     */
+    $(document).ready(function() {
+        var canvas = document.getElementById("png-example");
+        if(utils.doesElementExist(canvas)) {
+            var context = draw(canvas);
+            setInterval(function() { update(context); }, 20);
+        }
+    });
+
+    /**
+     * Utility functions.
+     */
+    var utils = {
+
+      /**
+       * Generate a random number between the min and the max.
+       */
+      getRandomNum : function(min, max) {
+        return Math.round(Math.random() * (max - min + 1)) + min;
+      },
+
+      /**
+       * Checks if the given element object exists.
+       */
+      doesElementExist : function(el) {
+        return (typeof(el) != 'undefined' && el !== null);
+      }
+    };
+
+})();
